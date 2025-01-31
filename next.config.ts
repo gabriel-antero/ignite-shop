b0fc5bda8900d9e5b0ca71bca21f75b6e6604dc1
Gabriel Antero <cgabrielantero@gmail.com>
2025-01-31 11:35:12 -0300

    add page home

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+
+  // experimental: {
+  //   images: {
+  //     allowFutureImage: true,
+  //   },
+  // },
 };
 
 export default nextConfig;
