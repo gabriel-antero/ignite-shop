07cb0ddf2b0cf607cd0bec6bae0dd8a89a08df34
Gabriel Antero <cgabrielantero@gmail.com>
2025-02-05 11:34:57 -0300

    using stripe dynamic products

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
+  images: {
+    domains: ["files.stripe.com"],
+  },
+
   // experimental: {
   //   images: {
   //     allowFutureImage: true,
